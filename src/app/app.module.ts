256113107fc53cd66679b3e1a4fcb96df7c1b086
Шишминцев Григорий Владимирович <GVSHISHM@mts.ru>
2018-03-22 16:47:26 +0300

    search card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		SearchComponent
+	],
+	imports: [
+		BrowserModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatInputModule,
+		BrowserAnimationsModule
+	],
+	providers: [],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
